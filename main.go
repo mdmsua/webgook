658d9d189ae0d921f582c5f576656a9dbd872b0a
Dmytro Morozov <mdmsua@icloud.com>
2024-06-05 10:13:28 +0200

    Log request body

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 var infoLog = log.New(os.Stdout, "[INFO] ", log.LstdFlags)
+var errorLog = log.New(os.Stderr, "[ERR] ", log.LstdFlags)
 var port = os.Getenv("PORT")
 
 func main() {
@@ -16,6 +18,12 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	infoLog.Printf("RequestURI: %s, RemoteAddr: %s", r.RequestURI, r.RemoteAddr)
+	infoLog.Printf("%s %s", r.Method, r.RequestURI)
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		errorLog.Println(err)
+	} else {
+		infoLog.Println(string(data))
+	}
 	w.WriteHeader(http.StatusNoContent)
 }
